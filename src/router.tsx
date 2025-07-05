f2814f153939e7844bb7c6ba1b2f904c670f8ede
Joao Mota <joaoefmota@gmail.com>
2025-07-05 19:16:14 +0200

    chore: setup done with query

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,23 @@
-// src/router.tsx
+import { QueryClient } from '@tanstack/react-query'
 import { createRouter as createTanStackRouter } from '@tanstack/react-router'
+import { routerWithQueryClient } from '@tanstack/react-router-with-query'
 import { routeTree } from './routeTree.gen'
+import { DefaultCatchBoundary } from './components/DefaultCatchBoundary'
+import { NotFound } from './components/NotFound'
 
 export function createRouter() {
-    const router = createTanStackRouter({
-        routeTree,
-        scrollRestoration: true, // This is used to restore the scroll position of the page when navigating between routes.
-    })
+    const queryClient = new QueryClient()
 
-    return router
+    return routerWithQueryClient(
+        createTanStackRouter({
+            routeTree,
+            context: { queryClient },
+            defaultPreload: 'intent',
+            defaultErrorComponent: DefaultCatchBoundary,
+            defaultNotFoundComponent: () => <NotFound />,
+        }),
+        queryClient,
+    )
 }
 
 declare module '@tanstack/react-router' {
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,19 @@
-// app/routes/__root.tsx
 import {
     Outlet,
-    createRootRoute,
     HeadContent,
     Scripts,
+    createRootRouteWithContext,
 } from '@tanstack/react-router'
 import type { ReactNode } from 'react'
+import { seo } from "../utils/seo"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { DefaultCatchBoundary } from "../components/DefaultCatchBoundary"
+import { NotFound } from "../components/NotFound"
+import { QueryClient } from "@tanstack/react-query"
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+    queryClient: QueryClient
+}>()({
     head: () => ({
         meta: [
             {
@@ -21,7 +27,25 @@ export const Route = createRootRoute({
                 title: 'TanStack Start Starter',
             },
         ],
+        links: [{
+            rel: 'stylesheet',
+            href: '/src/styles/globals.css',
+        }],
+        ...seo({
+            title: 'TanStack Start Starter',
+            description: 'A starter template for TanStack Start with React.',
+            image: 'https://tanstack.dev/start/og-image.png',
+            keywords: 'tanstack, start, react, starter, template',
+        })
     }),
+    errorComponent: (props) => {
+        return (
+            <RootDocument>
+                <DefaultCatchBoundary {...props} />
+            </RootDocument>
+        )
+    },
+    notFoundComponent: () => <NotFound />,
     component: RootComponent,
 })
 
@@ -41,6 +65,7 @@ function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
             </head>
             <body>
                 {children}
+                <ReactQueryDevtools buttonPosition="bottom-left" />
                 <Scripts />
             </body>
         </html>
